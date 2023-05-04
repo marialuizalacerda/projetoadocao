a34eced425ac3adebb88f2abec432fd57ee002f9
marialuizalacerda <lacerdamarialuiza1@gmail.com>
2023-05-04 09:27:53 -0300

    criando os card com js

--- script.js
+++ script.js
@@ -53,9 +53,52 @@ const Cadastro = {
     console.log(historia)
     console.log(protetor)
     console.log(contato)
+
+    NewCard.new(nome, especies, cidade, estado, historia)
+
   },
 
 };
+
+const NewCard = {
+  new(paramNome, paramEspecies, paramCidade, paramEstado, paramHistoria) {
+    // Pega a div lista-animais
+    const divList = document.getElementById('lista-animais')
+
+    //Cria uma nova div
+    const newDiv = document.createElement('div')
+    //Seta a classe card para a nova div criada
+    newDiv.className = 'card'
+
+    //Cria o elemento H3
+    const newH3 = document.createElement('h3')
+    //Passa o nome do animal para o H3
+    newH3.innerHTML = paramNome
+    //Insere o H3 como filho da nova div criada
+    newDiv.appendChild(newH3)
+
+    const newP = document.createElement('p')
+    newP.innerHTML = paramEspecies
+    newDiv.appendChild(newP)
+
+    const newLocalizacao = document.getElementById('localizacao')
+    newLocalizacao.innerHTML = paramCidade, paramEstado
+    newDiv.appendChild(newLocalizacao)
+
+    //Cria o elemento da história do bicho
+    const newH = document.getElementById('h')
+    //Passa a historia para o p 'historia'
+    newH.innerHTML = paramHistoria
+    //Insere o p 'historia' como filho da nova div criada
+    newDiv.appendChild(newH)
+    
+
+    divList.appendChild(newDiv)
+
+    //Fecha o modal após salvar
+    Divulgar.close()
+  }
+}
 
   
     
